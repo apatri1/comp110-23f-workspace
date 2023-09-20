bf1749e55d10e18e8e82873abbf110d424ec1196
Ashley Artica-Zuniga <apatri@edu.unc>
2023-09-19 20:20:36 -0400

    Wordle Lint Corrections

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -9,27 +9,27 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 word_idx: int = 0
 result: str = ""
-#In case the user writes a word < or > the secret word
+# In case the user writes a word < or > the secret word
 while (len(guess) > len(secret)) or (len(guess) < len(secret)):
     guess = input(f"That was not {len(secret)} letters! Try again: ")
 while word_idx < len(secret):
     if guess[word_idx] == secret[word_idx]:
         result += GREEN_BOX
-        #The user got this character correct, skips to the next character
+        # The user got this character correct, skips to the next character
     if guess[word_idx] != secret[word_idx]:
         chr_exists_alt: bool = False
         alt_idx: int = 0
-        while (chr_exists_alt == False) and (alt_idx < len(secret)):
-            if guess[word_idx] == secret [alt_idx]:
+        while (chr_exists_alt is False) and (alt_idx < len(secret)):
+            if guess[word_idx] == secret[alt_idx]:
                 chr_exists_alt = True
-        #It is true that this character exists somewhere in the secret word
+        # It is true that this character exists somewhere in the secret word
             else:
                 alt_idx += 1
-        if chr_exists_alt == True:
+        if chr_exists_alt is True:
             result += YELLOW_BOX
         else:
             result += WHITE_BOX
-            #This character is not in the word at all
+            # This character is not in the word at all
     word_idx += 1
 print(result)
 if guess == secret:
