952f63610efbcde101cce784c62985408ecc0aa1
Ashley Artica-Zuniga <apatri@edu.unc>
2023-09-23 09:37:25 -0400

    Emojified complete

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -12,4 +12,23 @@ def contains_char(search_str: str, sing_chr: str) -> bool:
         else:
             search_idx += 1
     return False      
-        
+
+def emojified(user_guess: str, secret_word: str) -> str:
+    """String of emoji boxes whose color determines the accuracy of the guess"""
+    assert len(user_guess) == len(secret_word)
+    result: str = ""
+    word_idx: int = 0
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    while word_idx < len(secret_word):
+        if secret_word[word_idx] == user_guess[word_idx]:
+            result += GREEN_BOX
+            word_idx += 1           
+        elif contains_char(secret_word, user_guess[word_idx]) == True:
+            result += YELLOW_BOX
+            word_idx += 1
+        else:
+            result += WHITE_BOX
+            word_idx += 1
+    return result
