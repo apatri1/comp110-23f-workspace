f1dd16df433736aa43e72287510283af11785d8b
Ashley Artica-Zuniga <apatri@edu.unc>
2023-11-14 09:52:31 -0500

    make points

--- lessons/CQ07/make_points.py
+++ lessons/CQ07/make_points.py
@@ -1,3 +1,5 @@
+"""Makes a point using Point class."""
+
 from lessons.CQ07.point import Point
 
 my_point: Point = Point(2.4, 5.8)
@@ -7,5 +9,9 @@ print(my_point.x)
 my_point.scale_by(2)
 print(my_point.x)
 
-new_point: Point = my_point.scale(4)
-print(new_point.x)
+new_point: Point = my_point * 2.0
+other_point: Point = Point()
+
+print(my_point)
+print(new_point)
+print(other_point)
--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -6,21 +6,35 @@ __author__ = "730656248"
 
 
 class Point:
+    """Class to represent (x,y) coordinate point."""
+
     x: float
     y: float
-    def __init__(self, x_init: float, y_init: float):
+
+    def __init__(self, x_init: float = 0.0, y_init: float = 0.0):
         """My constructor."""
         self.x = x_init
         self.y = y_init
 
+    def __str__(self) -> str:
+        """Magic method."""
+        output: str = f"x: {self.x}; y: {self.y}"
+        return output
     
     def scale_by(self, factor: int) -> None:
         """Mutates a point."""
-        self.x = self.x * factor
-        self.y = self.y * factor
-
+        self.x *= factor
+        self.y *= factor
 
     def scale(self, factor: int) -> Point:
         """Creates a new point."""
-        new_point: Point = Point(self.x * factor, self.y * factor)
-        return new_point
+        return Point(self.x * factor, self.y * factor) 
+
+    def __mul__(self, factor: int | float) -> Point:
+        """Operator overload, multiplication."""
+        return Point(self.x * factor, self.y * factor)   
+
+    def __add__(self, factor: int | float) -> Point:
+        """Operator overload, addition."""
+        return Point(self.x + factor, self.y + factor)
+        
